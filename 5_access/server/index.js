ee2f8a423cbf80a502ab792e4594ea8b76dcc2c8
aalu1418 <aalu1418@gmail.com>
2019-10-17 18:20:44 -0400

    finished 404 error integration

--- 5_access/server/index.js
+++ 5_access/server/index.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 const port = 3000;
 
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json()); // for parsing application/jsons
 
 try {
   app.get("/", (req, res) => {
@@ -13,7 +13,7 @@ try {
 
   //https://stackoverflow.com/questions/25623041/how-to-configure-dynamic-routes-with-express-js
   app.get("/:id", function(req, res) {
-    console.log(req.params.id);
+    // console.log(req.params.id);
     if (req.params.id != "aaron") {
       send_webpage(res, "access_denied");
     } else {
@@ -26,9 +26,13 @@ try {
     // res.sendFile(req.params.id);
   });
 
-  // app.use((req, res, next) => {
-  //   res.status(404).send("File Not Found");
-  // });
+  app.get("/:usrnm/secured/:id", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/secured", req.params.id));
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).send("File Not Found");
+  });
 
   app.listen(port, () => console.log(`[success] Ok on port ${port}`));
 } catch (e) {
