37c73b79004ad4f29f96ec498228f760fed412ab
aalu1418 <aalu1418@gmail.com>
2019-10-17 16:56:10 -0400

    almost complete - need to include 404 without breaking things

---
+++ 5_access/server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+var app = express();
+var bodyParser = require("body-parser");
+const port = 3000;
+
+app.use(bodyParser.json()); // for parsing application/json
+
+try {
+  app.get("/", (req, res) => {
+    send_webpage(res, "index");
+  });
+
+  //https://stackoverflow.com/questions/25623041/how-to-configure-dynamic-routes-with-express-js
+  app.get("/:id", function(req, res) {
+    console.log(req.params.id);
+    if (req.params.id != "aaron") {
+      send_webpage(res, "access_denied");
+    } else {
+      app.use(
+        "/" + req.params.id + "/secured",
+        express.static("../client/secured")
+      );
+      res.redirect("/" + req.params.id + "/secured/access_granted.html")
+    }
+    // res.sendFile(req.params.id);
+  });
+
+  // app.use((req, res, next) => {
+  //   res.status(404).send("File Not Found");
+  // });
+
+  app.listen(port, () => console.log(`[success] Ok on port ${port}`));
+} catch (e) {
+  console.log("[failed] " + e);
+} finally {
+}
+
+const send_webpage = (res, filename) => {
+  res.sendFile(path.join(__dirname, "../client/unsecured", filename + ".html"));
+};
